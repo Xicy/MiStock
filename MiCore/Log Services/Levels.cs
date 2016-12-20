d7fb376ad61d8f4a965b70054d885be20692abb5
Umut Akkaya <umutakkaya1996@gmail.com>
2016-12-20 18:45:53 +0300

    small bug fix

--- MiCore/Log Services/Levels.cs
+++ MiCore/Log Services/Levels.cs
@@ -7,14 +7,14 @@ namespace MiCore
         [Flags]
         public enum Level : byte
         {
-            Off = byte.MinValue,    //0
+            Off = Byte.MinValue,    //0
 
             Debug = 1 << 0,         //1
             Info = 1 << 1,          //2
             Warn = 1 << 2,          //4
             Error = 1 << 3,         //8
 
-            All = byte.MaxValue,    //255
+            All = Byte.MaxValue,    //255
         }
 
 
