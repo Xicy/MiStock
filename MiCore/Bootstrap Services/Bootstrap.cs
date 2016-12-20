c4f3f69c762b9d17604efcda3807a533b174ff9e
Umut Akkaya <umutakkaya1996@gmail.com>
2016-12-20 18:46:11 +0300

    preview change

--- MiCore/Bootstrap Services/Bootstrap.cs
+++ MiCore/Bootstrap Services/Bootstrap.cs
@@ -1,32 +1,33 @@
 ﻿
 namespace MiCore
 {
+    /* Log system
+     *  -Develop
+     *  
+     * Login Server
+     *  -Check License with global serial
+     *  -Check Update 
+     *  -Listen Server for request
+     *  
+     * Initalize Database
+     *  -Password cpu serial
+     *  -Check Database file if not exist create with default values
+     *  -Load All Modules
+     *  
+     * Initalize Modules for Api
+     *  -Reach all module other module
+     *  -
+     * 
+     * Create WebServer
+     *  -Check files from database if not exist request from Server
+     *  -Listen for request 
+     */
+
     public class Bootstrap
     {
-        public Bootstrap()
+        public static void Start()
         {
-            new SocketTest();
-            /* Log system
-             *  -Develop
-             *  
-             * Login Server
-             *  -Check License with global serial
-             *  -Check Update 
-             *  -Listen Server for request
-             *  
-             * Initalize Database
-             *  -Password cpu serial
-             *  -Check Database file if not exist create with default values
-             *  -Load All Modules
-             *  
-             * Initalize Modules for Api
-             *  -Reach all module other module
-             *  -
-             * 
-             * Create WebServer
-             *  -Check files from database if not exist request from Server
-             *  -Listen for request 
-             */
+            SocketTest.Start();
         }
     }
 }
