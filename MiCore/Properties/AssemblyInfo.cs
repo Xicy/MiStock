3a2a5e4eb16b2982ce7763a365cd74d10e7ec0df
Umut Akkaya <umutakkaya1996@gmail.com>
2016-12-20 18:17:08 +0300

    MiCore AssemblyInfo Update

--- MiCore/Properties/AssemblyInfo.cs
+++ MiCore/Properties/AssemblyInfo.cs
@@ -7,7 +7,7 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("MiCore")]
-[assembly: AssemblyDescription("Shared MiClient Files")]
+[assembly: AssemblyDescription("Shared MiCore Files")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("MiStock")]
