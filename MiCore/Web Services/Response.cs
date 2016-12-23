1c4234cb2cf631b7a2b03b42dfc258fa5fae05ee
Umut Akkaya <umutakkaya1996@gmail.com>
2016-12-23 20:05:26 +0300

    Response system building start

--- MiCore/Web Services/Response.cs
+++ MiCore/Web Services/Response.cs
@@ -1,4 +1,5 @@
-﻿using System.Text;
+﻿using System;
+using System.Text;
 
 namespace MiCore
 {
@@ -6,8 +7,12 @@ namespace MiCore
     {
         public class Response
         {
-            public byte[] ResponseData => Encoding.UTF8.GetBytes("Hello from server");
-            protected internal Request RequestData;
+            //TODO:Response
+            public byte[] ResponseData => Encoding.UTF8.GetBytes(_return);
+
+            private static string _returnBin =
+                "<!DOCTYPE html>\r\n<html>\r\n<body>\r\n\r\n<h1>My First Heading</h1>\r\n<p>My first paragraph.</p>\r\n\r\n</body>\r\n</html>";
+            private string _return = $"HTTP/1.1 200 OK\r\nDate: {DateTime.Now:r}\r\nServer: {typeof(Bootstrap).Namespace}\r\nLast-Modified: {DateTime.Now:r}\r\nContent-Length: {_returnBin.Length}\r\nContent-Type: text/html\r\nConnection: Closed\r\n\r\n{_returnBin}";
         }
     }
 }
