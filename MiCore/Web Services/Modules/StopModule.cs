05101195ccf8bffd87f21ab4800449308a3a34f9
Umut Akkaya <umutakkaya1996@gmail.com>
2016-12-31 02:35:44 +0300

    Add for testing stop module

---
+++ MiCore/Web Services/Modules/StopModule.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace MiCore
+{
+    internal partial class WebSocket
+    {
+        public class StopModule : IWebModule
+        {
+            public string RegexPath => @"^\/stop$";
+            public Response Execute(WebSocket socket, Request request)
+            {
+                socket.Stop();
+                return new Response(404);
+            }
+        }
+    }
+}
