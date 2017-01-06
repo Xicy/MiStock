60fa3d8e7aa7391fbe6df982c05c622015d1a912
Umut Akkaya <umutakkaya1996@gmail.com>
2017-01-07 00:29:58 +0300

    cookie module ok

--- MiCore/Web Services/Cookie/CookieCollection.cs
+++ MiCore/Web Services/Cookie/CookieCollection.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text.RegularExpressions;
 
 namespace MiCore
 {
@@ -8,11 +9,52 @@ namespace MiCore
     {
         public class CookieCollection
         {
-            public IEnumerable<CookieContainer> Cookies;
+            private IList<CookieContainer> _cookies;
+            public IEnumerable<CookieContainer> Cookies => _cookies;
+
+            public CookieCollection()
+            {
+                _cookies = new List<CookieContainer>();
+            }
+            public CookieCollection(IList<CookieContainer> cookies)
+            {
+                _cookies = cookies;
+            }
+
+            public bool IsNull() => _cookies.Count <= 0;
+
+            public CookieCollection Add(CookieContainer cookie)
+            {
+                _cookies.Add(cookie);
+                return this;
+            }
+
+            public static CookieCollection ToCookieCollection(string cookies)
+            {
+
+                var matches = new Regex(@"(?<key>[^\s*]*?)=(?<value>[^;]*)|(?<secure>[S|s]ecure)|(?<httponly>[H|h]ttp[O|o]nly)").Matches(cookies).Cast<Match>();
+
+                var _Expires = matches.Count(a => a.Groups["key"].Value.ToLowerInvariant() == "expires") > 0 ? DateTime.Parse(matches.First(a => a.Groups["key"].Value.ToLowerInvariant() == "expires").Groups["value"].Value) : DateTime.MinValue;
+                var _MaxAge = matches.Count(a => a.Groups["key"].Value.ToLowerInvariant() == "max-age") > 0 ? TimeSpan.FromSeconds(double.Parse(matches.First(a => a.Groups["key"].Value.ToLowerInvariant() == "max-age").Groups["value"].Value)) : TimeSpan.Zero;
+                var _Path = matches.Count(a => a.Groups["key"].Value.ToLowerInvariant() == "path") > 0 ? matches.First(a => a.Groups["key"].Value.ToLowerInvariant() == "path").Groups["value"].Value : string.Empty;
+                var _Secure = matches.Any(a => a.Groups["secure"].Success);
+                var _HttpOnly = matches.Any(a => a.Groups["httponly"].Success);
+
+                var keyvalue = matches.Where(
+                    a =>
+                        a.Groups["key"].Value.ToLowerInvariant() != "path" &&
+                        a.Groups["key"].Value.ToLowerInvariant() != "expires" &&
+                        a.Groups["key"].Value.ToLowerInvariant() != "max-age" &&
+                        !a.Groups["secure"].Success &&
+                        !a.Groups["httponly"].Success
+                        ).Select(a => new CookieContainer(a.Groups["key"].Value, a.Groups["value"].Value) { Expires = _Expires, HttpOnly = _HttpOnly, MaxAge = _MaxAge, Path = _Path, Secure = _Secure });
+
+                return new CookieCollection(keyvalue.ToList());
+            }
 
             public string ToResponseData()
             {
-                return Cookies.Aggregate("", (current, pair) => current + (pair.ToResponseData() + Environment.NewLine));
+                return Cookies.Aggregate("", (current, pair) => current + pair.ToResponseData() + Environment.NewLine).TrimEnd('\r', '\n');
             }
 
         }
--- MiCore/Web Services/Cookie/CookieContainer.cs
+++ MiCore/Web Services/Cookie/CookieContainer.cs
@@ -1,7 +1,5 @@
 ﻿using System;
-using System.Linq;
-using System.Collections.Generic;
-using System.Text.RegularExpressions;
+using System.IO;
 
 namespace MiCore
 {
@@ -9,7 +7,8 @@ namespace MiCore
     {
         public class CookieContainer
         {
-            public KeyValuePair<string, string> Cookies;
+            public string Key;
+            public string Value;
             public string Path;
             public DateTime Expires;
             public TimeSpan MaxAge;
@@ -17,34 +16,20 @@ namespace MiCore
             public bool Secure;
             public bool HttpOnly;
 
-            //TODO: expires and max-age tostring()
-            public static CookieContainer ToCookie(string cookie)
+            public CookieContainer(string key, string value)
             {
-                var matches = new Regex(@"(?<key>[^\s*]*?)=(?<value>[^;]*)|(?<secure>[S|s]ecure)|(?<httponly>[H|h]ttp[O|o]nly)").Matches(cookie).Cast<Match>();
-
-                var keyvalue = matches.Where(
-                    a =>
-                        a.Groups["key"].Value.ToLowerInvariant() != "path" &&
-                        a.Groups["key"].Value.ToLowerInvariant() != "expires" &&
-                        a.Groups["key"].Value.ToLowerInvariant() != "max-age" &&
-                        !a.Groups["secure"].Success &&
-                        !a.Groups["httponly"].Success
-                        ).Select(a => new KeyValuePair<string, string>(a.Groups["key"].Value, a.Groups["value"].Value));
-
-                return new CookieContainer
+                if (key.ToLowerInvariant() == "expires" || key.ToLowerInvariant() == "max-age" || key.ToLowerInvariant() == "path" || key.ToLowerInvariant() == "secure" || key.ToLowerInvariant() == "httponly")
                 {
-                    //Cookies = keyvalue,
-                    Expires = matches.Count(a => a.Groups["key"].Value.ToLowerInvariant() == "expires") > 0 ? DateTime.Parse(matches.First(a => a.Groups["key"].Value.ToLowerInvariant() == "expires").Groups["value"].Value) : DateTime.MinValue,
-                    MaxAge = matches.Count(a => a.Groups["key"].Value.ToLowerInvariant() == "max-age") > 0 ? TimeSpan.FromSeconds(double.Parse(matches.First(a => a.Groups["key"].Value.ToLowerInvariant() == "max-age").Groups["value"].Value)) : TimeSpan.Zero,
-                    Path = matches.Count(a => a.Groups["key"].Value.ToLowerInvariant() == "path") > 0 ? matches.First(a => a.Groups["key"].Value.ToLowerInvariant() == "path").Groups["value"].Value : string.Empty,
-                    Secure = matches.Any(a => a.Groups["secure"].Success),
-                    HttpOnly = matches.Any(a => a.Groups["httponly"].Success)
-                };
+                    throw new ArgumentException("Invalid Key name. Please dont use 'expires', 'max-age', 'path', 'secure', 'httponly'");
+                }
+
+                Key = key;
+                Value = value;
             }
 
             public string ToResponseData()
             {
-                return $"Set-Cookie:{Cookies.Key}={Cookies.Value};{(string.IsNullOrEmpty(Path) ? "" : $" Path={Path};")}{(Expires == DateTime.MinValue ? "" : $" Expires={Expires:r};")}{(MaxAge == TimeSpan.Zero ? "" : $" Max-Age={MaxAge.TotalSeconds};")}{(Secure ? " Secure;" : "")}{(HttpOnly ? " HttpOnly;" : "")}";
+                return $"Set-Cookie:{Key}={Value};{(string.IsNullOrEmpty(Path) ? "" : $" Path={Path};")}{(Expires == DateTime.MinValue ? "" : $" Expires={Expires:r};")}{(MaxAge == TimeSpan.Zero ? "" : $" Max-Age={MaxAge.TotalSeconds};")}{(Secure ? " Secure;" : "")}{(HttpOnly ? " HttpOnly;" : "")}";
             }
 
         }
