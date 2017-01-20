477096bc867e86ae7f7d3005ed6c7a5aaef3f272
Umut Akkaya <umutakkaya1996@gmail.com>
2017-01-21 00:41:53 +0300

    Added matche variable

--- MiCore/Web Services/Modules/IndexModule.cs
+++ MiCore/Web Services/Modules/IndexModule.cs
@@ -9,10 +9,10 @@ namespace MiCore
         public class IndexModule : IWebModule
         {
             public string RegexPath => @"^(\/|\/index)$";
+            public Match Match { set; get; }
             public Response Execute(WebSocket socket, Request request)
             {
-                return new Response(File.OpenRead(@"D:\Downloads\a.png"));
-                //return new Response(Encoding.UTF8.GetBytes("<!DOCTYPE html>\r\n<html>\r\n<body>\r\n\r\n<h1>My First Heading</h1>\r\n<p>My first paragraph.</p>\r\n\r\n</body>\r\n</html>"), ".html");
+                return new Response(Encoding.UTF8.GetBytes("<!DOCTYPE html>\r\n<html>\r\n<body>\r\n\r\n<h1>My First Heading</h1>\r\n<p>My first paragraph.</p>\r\n\r\n</body>\r\n</html>"), ".html");
             }
         }
     }
