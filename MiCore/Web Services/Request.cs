d2a734b53fe976926545eac1a8f6e2452f2c35f4
Umut Akkaya <umutakkaya1996@gmail.com>
2017-01-21 00:43:38 +0300

    Fixed content spliting Added header Add some header methodes

--- MiCore/Web Services/Request.cs
+++ MiCore/Web Services/Request.cs
@@ -1,4 +1,6 @@
 ﻿using System.Collections.Generic;
+using System.Linq;
+using System.Text;
 using System.Text.RegularExpressions;
 
 namespace MiCore
@@ -7,39 +9,55 @@ namespace MiCore
     {
         public class Request
         {
-            private readonly string _data;
+            //TODO:overwrite ToString()
+            private string Header;
 
             public readonly Dictionary<string, string> Data;
             public string Method => Data["method"];
-            public string Path => Data["path"].Substring(0, Data["path"].Length - 9);
-            public string Content => Data.ContainsKey("content-length") ? _data.Substring(_data.Length - int.Parse(Data["content-length"]), int.Parse(Data["content-length"])) : "";
+            public string Path => Data["path"];
+            public IEnumerable<byte> Content;
             public CookieCollection Cookies => Data.ContainsKey("cookie") ? CookieCollection.ToCookieCollection(Data["cookie"]) : null;
-            public Request(string data)
+
+            public override string ToString()
             {
+                return Header;
+            }
+
+            public Request(ref IEnumerable<byte> data)
+            {
+                var refData = data.ToArray();
+                var strData = Encoding.UTF8.GetString(refData);
+
                 Data = new Dictionary<string, string>();
-                _data = data;
+
                 var regex = new Regex(@"^(?<Key>.*?):?\s(?<Value>.*?)\r?$", RegexOptions.Multiline);
-                var matches = regex.Matches(_data);
+                var regexLen = new Regex(@"^[C|c]ontent-[L|l]ength:\s*?(.*?)$", RegexOptions.Multiline).Match(strData).Groups[1];
+
+                var contentLen = int.Parse(regexLen.Success ? regexLen.Value : "0");
+
+                var headerLen = refData.Length - contentLen;
+                Content = refData.Skip(headerLen).Take(contentLen);
+                Header = Encoding.UTF8.GetString(refData.Take(headerLen).ToArray());
+                var matches = regex.Matches(Header);
+
                 foreach (Match match in matches)
                 {
                     switch (match.Groups["Key"].Value.ToLowerInvariant())
                     {
+                        case "options":
                         case "get":
+                        case "head":
                         case "post":
                         case "put":
                         case "delete":
                             Data.Add("method", match.Groups["Key"].Value.ToLowerInvariant());
-                            Data.Add("path", match.Groups["Value"].Value);
+                            Data.Add("path", match.Groups["Value"].Value.Substring(0, match.Groups["Value"].Value.Length - 9));
                             break;
                         default: Data.Add(match.Groups["Key"].Value.ToLowerInvariant(), match.Groups["Value"].Value); break;
                     }
                 }
             }
 
-            public override string ToString()
-            {
-                return _data;
-            }
         }
     }
 }
