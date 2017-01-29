58cfe7da3c3663f6a01bc5603654f972ead267f2
Umut Akkaya <umutakkaya1996@gmail.com>
2017-01-29 14:32:32 +0300

    testing file responding

--- MiCore/Web Services/Modules/FileModule.cs
+++ MiCore/Web Services/Modules/FileModule.cs
@@ -1,4 +1,7 @@
-﻿using System.IO;
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
 using System.Text;
 using System.Text.RegularExpressions;
 
@@ -12,9 +15,28 @@ namespace MiCore
             public Match Match { set; get; }
             public Response Execute(WebSocket socket, Request request)
             {
-                return File.Exists(@"D:\Pictures\Screenshots\" + Match.Groups[1] + "." + Match.Groups[2])
-                    ? new Response(File.OpenRead(@"D:\Pictures\Screenshots\" + Match.Groups[1] + "." + Match.Groups[2]))
-                    : new Response(404);
+                var response = new Response(404);
+                if (File.Exists(@"D:\Pictures\Screenshots\" + Match.Groups[1] + "." + Match.Groups[2]))
+                {
+                    response = new Response(202, GetFile(), "." + Match.Groups[2]);
+                }
+                return response;
+            }
+
+            private IEnumerable<byte> GetFile()
+            {
+                var file = File.OpenRead(@"D:\Pictures\Screenshots\" + Match.Groups[1] + "." + Match.Groups[2]);
+                var Enum = Enumerable.Empty<byte>();
+                var buffer = new byte[1024];//1KB
+                for (var i = 0; i < file.Length; i += 1024)
+                {
+                    var read = Math.Min(1024, (int) file.Length - i);
+
+                    file.Read(buffer, i, read);
+
+                    Enum = Enum.Concat(buffer.Take(read));
+                }
+                return Enum;
             }
         }
     }
