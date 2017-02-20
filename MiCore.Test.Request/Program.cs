7bb9d017050e2fb51fa14d6594623c97cd59e2ac
Umut Akkaya <umutakkaya1996@gmail.com>
2017-02-20 14:48:37 +0300

    added multi request test projection

---
+++ MiCore.Test.Request/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MiCore.Test.Request
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Parallel.For(1, 1000, i =>
+            {
+                try
+                {
+                    WebRequest.Create($"http://127.0.0.1:8080/{i % 5}.png").GetResponse();
+
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(e);
+                }
+
+            });
+
+        }
+    }
+}
